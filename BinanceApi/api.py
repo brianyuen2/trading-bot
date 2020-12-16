57275cea736a5a39dd95d6aac5c0bb5f27d1a11f
Brian <brianyuen2@gmail.com>
2020-12-16 21:05:24 +1100

    Create order works

--- BinanceApi/api.py
+++ BinanceApi/api.py
@@ -2,7 +2,24 @@ from binance.client import Client
 import constants
 client = Client(constants.API_KEY, constants.API_SECRET)
 
+def get_eth_usdt_price():
+    return client.get_symbol_ticker(symbol="ETHUSDT")['price']
 
-# print(client.get_symbol_info('ETHUSDT'))
-price = client.get_symbol_ticker(symbol="ETHUSDT")
-print('Price for ETH/USDT is: ' + price['price'])
+def get_eth_balance():
+    balances = client.get_account()['balances']
+    for asset in balances:
+        if asset['asset'] == 'ETH':
+            return asset
+
+def create_new_eth_order():
+    return client.create_order(
+    symbol='ETHUSDT',
+    side='SELL',
+    type='LIMIT',
+    timeInForce='GTC',
+    quantity=0.05,
+    price=900)
+
+print(get_eth_balance())
+# print(create_new_eth_order())
+print(client.get_open_orders())
