88b003d076db32cd9f232cd54bf8f668e3ebb1ed
Brian <brianyuen2@gmail.com>
2020-12-21 16:50:47 +1100

    Created bot loop cycle and added comments

--- BinanceApi/api.py
+++ BinanceApi/api.py
@@ -1,25 +1,47 @@
 from binance.client import Client
 import constants
+import time
+
 client = Client(constants.API_KEY, constants.API_SECRET)
 
+# Returns eth/usdt current price
 def get_eth_usdt_price():
     return client.get_symbol_ticker(symbol="ETHUSDT")['price']
 
-def get_eth_balance():
+# Returns eth and usdt balances for the account in an array
+def get_eth_usdt_balance():
     balances = client.get_account()['balances']
+    assets = []
     for asset in balances:
-        if asset['asset'] == 'ETH':
-            return asset
+        if asset['asset'] == 'ETH' or asset['asset'] == 'USDT':
+            assets.append(asset)
+    return assets
 
-def create_new_eth_order():
-    return client.create_order(
+# Creates a new order for eth/usdt
+# takes in side and quantity
+def create_new_eth_order(side, quantity):
+    # Remove test when real trades are wanted
+    return client.create_test_order(
     symbol='ETHUSDT',
-    side='SELL',
-    type='LIMIT',
-    timeInForce='GTC',
-    quantity=0.05,
-    price=900)
+    side=side,
+    type='MARKET',
+    quantity=quantity,
+)
 
-print(get_eth_balance())
-# print(create_new_eth_order())
-print(client.get_open_orders())
+# Main logic function, pulls information from account and exchange before
+# deciding to trade or not
+def attempt_trade():
+    # if certain indicators are __ 
+    balance = get_eth_usdt_balance()
+    print(balance)
+
+# Bot loop cycle
+# attempts to make a trade every 30 sec
+def run_bot():
+    while(1):
+        attempt_trade()
+        time.sleep(30)
+
+
+run_bot()
+# create_new_eth_order('SELL', 0.1)
