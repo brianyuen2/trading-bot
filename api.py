174dcab0890c2981230c8889e9ef91776594fbac
Brian <brianyuen2@gmail.com>
2020-12-22 19:22:28 +1100

    Basic logging added

--- api.py
+++ api.py
@@ -3,6 +3,10 @@ from binance.enums import *
 from binance.exceptions import BinanceAPIException, BinanceOrderException
 import constants
 import time
+import logging
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(message)s',
+ datefmt='%d-%b-%y %H:%M:%S')
 
 client = Client(constants.API_KEY, constants.API_SECRET)
 
@@ -11,16 +15,19 @@ def get_eth_usdt_price():
     try:
         return client.get_symbol_ticker(symbol="ETHUSDT")['price']
     except:
-        print('Error getting price')
+        logging.error('Error getting price')
 
 # Returns eth and usdt balances for the account in an array
 def get_eth_usdt_balance():
-    balances = client.get_account()['balances']
-    assets = []
-    for asset in balances:
-        if asset['asset'] == 'ETH' or asset['asset'] == 'USDT':
-            assets.append(asset)
-    return assets
+    try:
+        balances = client.get_account()['balances']
+        assets = []
+        for asset in balances:
+            if asset['asset'] == 'ETH' or asset['asset'] == 'USDT':
+                assets.append(asset)
+        return assets
+    except:
+        logging.error('Error getting balances')
 
 # Creates a new order for eth/usdt
 # takes in side and quantity
@@ -32,13 +39,13 @@ def create_new_eth_order(side, quantity):
         side=side,
         type='MARKET',
         quantity=quantity,
+        price=900
     )
     except BinanceAPIException as e:
         # error handling goes here
-        print(e)
+        logging.error(e)
     except BinanceOrderException as e:
         # error handling goes here
-        print(e)
+        logging.error(e)
 
-# run_bot()
 # create_new_eth_order('SELL', 0.1)
